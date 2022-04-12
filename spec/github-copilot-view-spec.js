996e54c742e04ea2b80a557a43af43bb7e37cd30
gresm <78505251+gresm@users.noreply.github.com>
2022-04-12 16:16:32 +0200

    Base Commit

---
+++ spec/github-copilot-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GithubCopilotView from '../lib/github-copilot-view';
+
+describe('GithubCopilotView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
